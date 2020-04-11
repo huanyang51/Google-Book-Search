19be4e0f7211e268f09da040b99d54980ac77385
hycoding51 <yanghuan.bnu@gmail.com>
2020-04-10 23:20:17 -0400

    feat: enable react app for the front-end and Navbar is added

---
+++ front-end/src/components/NavBar/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/search">
+          Google Books
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/search">
+                Search
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/saved">
+                Saved
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
